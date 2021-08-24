eb74df35044e5796284e599fcc4e3138d865dfa3
Wang Jun <34850979+WangJun2023@users.noreply.github.com>
2021-08-24 15:42:24 +0800

    Update RLFFC.m

--- RLFFC/RLFFC.m
+++ RLFFC/RLFFC.m
@@ -1,56 +1,70 @@
 function [F,R,gamma,obj] = RLFFC(YP,k,lambda,F_ba,beta,L_ba)
 
-num = size(YP, 1); %the number of samples
-numker = size(YP, 3); %m represents the number of kernels
-maxIter = 100; %the number of iterations
-gamma = ones(numker,1)/(numker);
-R = zeros(k,k,numker);
+num = size(YP, 1); % the number of bands
+view_num = size(YP, 3); % the number of superpixels
+maxIter = 100; % the number of iterations
 
-for p=1:numker % m - kernels
-    R(:,:,p) = eye(k);
+%% Initialization gamma, R, Y, F
+gamma = ones(view_num,1)/(view_num);
+
+R = zeros(k,k,view_num);
+for v = 1 : view_num
+    R(:,:,v) = eye(k);
 end
 
-Y_ba = zeros(num,k); % k - clusters, N - samples
+Y_ba = zeros(num,k); 
+
 [F,~] = qr(randn(num,k),0);
+
 opts = [];  opts.info = 1;
 opts.gtol = 1e-5;
 
 flag = 1;
 iter = 0;
+
+%% Iterative Update
 while flag
     iter = iter +1;
-    for p=1:numker
-        Y_ba = Y_ba + gamma(p)*(YP(:,:,p)*R(:,:,p));
+    for v = 1 : view_num
+        Y_ba = Y_ba + gamma(v)*(YP(:,:,v)*R(:,:,v));
     end
+    
+    %% update F
     X = F;
     A = - beta * L_ba;
     G = - Y_ba - lambda * F_ba;
     [F,~] = FOForth(X,G,@fun,opts,A,G); %calculate the minimum
    
-    for p=1:numker
-        if gamma(p)>1e-4
-            TP = gamma(p)*YP(:,:,p)'*(F);
-            [Up,Sp,Vp] = svd(TP,'econ');
-            R(:,:,p) = Up*Vp';
+   %% update R
+    for v = 1 : view_num
+        if gamma(v)>1e-4
+            temp_matrix = gamma(v)*YP(:,:,v)'*(F);
+            [Up,Sp,Vp] = svd(temp_matrix,'econ');
+            R(:,:,v) = Up*Vp';
         end
-    end 
-    coef = zeros(1,numker);
-    for p=1:numker
-        coef(1,p) = trace((F)'*YP(:,:,p)* R(:,:,p));
+    end
+    
+    %% update gamma
+    coef = zeros(1,view_num);
+    for v = 1 : view_num
+        coef(1,v) = trace((F)'*YP(:,:,v)* R(:,:,v));
     end
     gamma = coef/norm(coef,2); 
+    
+    %% calculate objective function value
     temp = zeros(num,k);
-    for p=1:numker
-        temp = temp + gamma(p)*(YP(:,:,p)*R(:,:,p));
+    for v = 1 : view_num
+        temp = temp + gamma(v)*(YP(:,:,v)*R(:,:,v));
     end
     obj(iter) = trace(F'*temp + lambda * F' * F_ba + beta * F' * L_ba * F);
-    
+   
+    %% judge convergence
     if (iter>2) && (abs((obj(iter-1)-obj(iter))/(obj(iter-1)))<1e-6 || iter>maxIter)
         flag =0;
     end
 end
 
-F = F./ repmat(sqrt(sum(F.^2, 2)), 1,k);
+    F = F./ repmat(sqrt(sum(F.^2, 2)), 1, k);
 
     function [funX, F] = fun(X,A,G)
         F = A * X + G;
