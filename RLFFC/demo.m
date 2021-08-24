2e7dc07e62a2c6f72bc9ad23457c035dc71ea145
Wang Jun <34850979+WangJun2023@users.noreply.github.com>
2021-08-24 15:49:37 +0800

    Update demo.m

--- RLFFC/demo.m
+++ RLFFC/demo.m
@@ -4,11 +4,11 @@ warning off;
 
 load('Indian_Pines-40.mat');
 
-H = LP(X1);
+H = LP(X1); % calculate the laplacian matrix of each segmented region
 
-d = 5;
+d = 5; % the latent feature dimension
 
-clusternum = 10 ;
+clusternum = 10; % the selected number of bands
 
 view_num = size(H,3);
 num = size(H,1);
@@ -36,5 +36,5 @@ beta = 0.1;
 [F,~,~,~] = RLFFC(HP,d,lambda,Y_ba,beta,L_ba);
 for k = 1 : length(clusternum)
     [IDX, C, SUMD, D] = kmeans(F,clusternum(k),'maxiter',100,'replicates',50,'emptyaction','singleton');
-    [~,I] = min(D);
+    [~,I] = min(D); % I: the selected band subset
 end
