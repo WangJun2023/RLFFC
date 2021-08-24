660feb0135adbc4855e8c34352a2bb4779405878
Wang Jun <34850979+WangJun2023@users.noreply.github.com>
2021-08-24 15:27:33 +0800

    Update demo.m

--- RLFFC/demo.m
+++ RLFFC/demo.m
@@ -6,34 +6,35 @@ load('Indian_Pines-40.mat');
 
 H = LP(X1);
 
-numclass = 5;
+d = 5;
 
 clusternum = 10 ;
 
-numker = size(H,3);
+view_num = size(H,3);
 num = size(H,1);
 L_ba = zeros(num,num);
 
-for i = 1 : numker
-    L_ba = L_ba + (1 / numker) * H(:,:,i);
+for i = 1 : view_num
+    L_ba = L_ba + (1 / view_num) * H(:,:,i);
 end
 
 H0 = zeros(num,num);
-qnorm = 2;
+
 opt.disp = 0;
-for p=1:numker
+for p=1:view_num
     H(:,:,p) = (H(:,:,p)+H(:,:,p)')/2;
-    [Hp, ~] = eigs(H(:,:,p), numclass, 'la', opt);
-    H0 = H0 + (1/numker)*H(:,:,p);
+    [Hp, ~] = eigs(H(:,:,p), d, 'la', opt);
+    H0 = H0 + (1/view_num)*H(:,:,p);
     HP(:,:,p) = Hp;
 end
-[Y_ba,~] = eigs(H0, numclass, 'la', opt);
+[Y_ba,~] = eigs(H0, d, 'la', opt);
 lambda = 2.^-15;
 beta = 0.1;
+%lambda = 2.^[-15:1:15];
+%beta = 0.01:0.01:0.1;
 
-
-   [F,~,~,~] = RLFFC(HP,numclass,lambda,Y_ba,beta,L_ba);
-        for k = 1 : length(clusternum)
-            [IDX, C, SUMD, D] = kmeans(F,clusternum(k),'maxiter',100,'replicates',50,'emptyaction','singleton');
-            [~,I] = min(D);
-        end
+[F,~,~,~] = RLFFC(HP,d,lambda,Y_ba,beta,L_ba);
+for k = 1 : length(clusternum)
+    [IDX, C, SUMD, D] = kmeans(F,clusternum(k),'maxiter',100,'replicates',50,'emptyaction','singleton');
+    [~,I] = min(D);
+end
